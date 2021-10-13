ac5ace11adccca4479e120606038c8d22747818f
Felipe Pilz <felipevilvertpilz@gmail.com>
2021-10-13 07:40:08 -0300

    Started ediaristas-maven-db project

---
+++ src/main/java/br/com/treinaweb/ediaristasmavendb/EdiaristasMavenDbApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.ediaristasmavendb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdiaristasMavenDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdiaristasMavenDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinaweb/ediaristasmavendb/EdiaristasMavenDbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.ediaristasmavendb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdiaristasMavenDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
