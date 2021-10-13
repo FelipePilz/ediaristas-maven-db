a0b4b73568b4859fd82bbf503ba1983c5000a641
Felipe Pilz <felipevilvertpilz@gmail.com>
2021-10-13 11:42:31 -0300

    Added first ModelAndView method to add a new Diarista

--- src/main/java/br/com/treinaweb/ediaristasmavendb/controllers/DiaristaController.java
+++ src/main/java/br/com/treinaweb/ediaristasmavendb/controllers/DiaristaController.java
@@ -1,10 +1,23 @@
 package br.com.treinaweb.ediaristasmavendb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.ediaristasmavendb.models.Diarista;
 
 @Controller
 @RequestMapping("/admin/diaristas")
 public class DiaristaController {
-	
+	//Show page
+
+	@GetMapping("/cadastrar")
+	public ModelAndView cadastrar(){
+		var modelAndView = new ModelAndView("admin/diaristas/form");//Located in
+		
+		modelAndView.addObject("diarista", new Diarista());
+		
+		return modelAndView;
+	}
 }
