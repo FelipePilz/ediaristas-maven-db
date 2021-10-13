36cadea174fc202b42ccc6811c740414d4a022a4
Felipe Pilz <felipevilvertpilz@gmail.com>
2021-10-13 11:50:49 -0300

    Added a post method cadastrar to insert a new diarista

--- src/main/java/br/com/treinaweb/ediaristasmavendb/controllers/DiaristaController.java
+++ src/main/java/br/com/treinaweb/ediaristasmavendb/controllers/DiaristaController.java
@@ -1,16 +1,22 @@
 package br.com.treinaweb.ediaristasmavendb.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.treinaweb.ediaristasmavendb.models.Diarista;
+import br.com.treinaweb.ediaristasmavendb.repositories.DiaristaRepository;
 
 @Controller
 @RequestMapping("/admin/diaristas")
 public class DiaristaController {
 	//Show page
+
+	@Autowired
+	private DiaristaRepository repository;
 
 	@GetMapping("/cadastrar")
 	public ModelAndView cadastrar(){
@@ -19,5 +25,11 @@ public class DiaristaController {
 		modelAndView.addObject("diarista", new Diarista());
 		
 		return modelAndView;
+	}
+
+	@PostMapping("/cadastrar")
+	public String cadastrar(Diarista diarista){
+		repository.save(diarista);
+		return "redirect:/admin/diaristas/cadastrar";
 	}
 }
