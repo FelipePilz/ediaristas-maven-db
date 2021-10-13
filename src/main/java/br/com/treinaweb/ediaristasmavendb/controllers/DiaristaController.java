b5e2687a4970437b9829a0a5278f6ec5f6b32f04
Felipe Pilz <felipevilvertpilz@gmail.com>
2021-10-13 11:35:51 -0300

    Created controller for Diarista

---
+++ src/main/java/br/com/treinaweb/ediaristasmavendb/controllers/DiaristaController.java
@@ -0,0 +1,10 @@
+package br.com.treinaweb.ediaristasmavendb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/diaristas")
+public class DiaristaController {
+	
+}
