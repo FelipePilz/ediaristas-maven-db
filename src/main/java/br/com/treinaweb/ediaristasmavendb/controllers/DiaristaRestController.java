e66e2823784b34b6848821a23cfa60cc6a627ecc
Felipe Pilz <felipevilvertpilz@gmail.com>
2021-10-16 00:48:21 -0300

    Added in API, a filter to search from IBGE

--- src/main/java/br/com/treinaweb/ediaristasmavendb/controllers/DiaristaRestController.java
+++ src/main/java/br/com/treinaweb/ediaristasmavendb/controllers/DiaristaRestController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.treinaweb.ediaristasmavendb.models.Diarista;
 import br.com.treinaweb.ediaristasmavendb.repositories.DiaristaRepository;
+import br.com.treinaweb.ediaristasmavendb.services.ViaCepService;
 
 @RestController
 @RequestMapping("/api/diaristas-cidade")
@@ -17,8 +19,16 @@ public class DiaristaRestController {
 	@Autowired
 	private DiaristaRepository repository;
 
+	@Autowired
+	private ViaCepService viaCepService;
+
 	@GetMapping
-	public List<Diarista> buscarDiaristasPorCep(){
-		return repository.findAll();
+	public List<Diarista> buscarDiaristasPorCep(@RequestParam String cep){
+		
+		var endereco = viaCepService.buscarEnderecoPorCep(cep);
+		var codigoIbge = endereco.getIbge();
+
+		
+		return repository.findByCodigoIbge(codigoIbge);
 	}
 }
--- src/main/java/br/com/treinaweb/ediaristasmavendb/repositories/DiaristaRepository.java
+++ src/main/java/br/com/treinaweb/ediaristasmavendb/repositories/DiaristaRepository.java
@@ -1,9 +1,13 @@
 package br.com.treinaweb.ediaristasmavendb.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.treinaweb.ediaristasmavendb.models.Diarista;
 
 public interface DiaristaRepository extends JpaRepository<Diarista, Long>{
 	
+	List<Diarista> findByCodigoIbge(String codigoIbge);
+
 }
